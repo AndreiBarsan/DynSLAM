24d89c1092840ad0caadbe167144139048161517
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2017-05-20 19:15:56 +0200

    Project tweaks to make it build.

--- src/DynSLAM/DynSLAM.cpp
+++ src/DynSLAM/DynSLAM.cpp
@@ -4,6 +4,11 @@
 #include <opencv/cv.h>
 #include <highgui.h>
 
+// TODO(andrei): Configure InfiniTAM's includes so we can be less verbose here.
+#include "../InfiniTAM/InfiniTAM/ITMLib/Objects/ITMView.h"
+#include "../InfiniTAM/InfiniTAM/ITMLib/Engine/ITMMainEngine.h"
+
+
 namespace dynslam {
   using namespace std;
 
@@ -12,8 +17,14 @@ namespace dynslam {
     try {
       cv::Mat img = cv::imread("/home/andrei/Pictures/george.jpg");
       cout << "Dimensions: " << img.cols << "x" << img.rows << endl;
+
+      Vector2i img_size(640, 480);
+      cout << "ITM Vector2i: " << img_size.x << ", " << img_size.y << endl;
+
       cv::imshow("The Summer of George", img);
       cv::waitKey(0);
+
+//      ITMLib::Engine::ITMMainEngine engine(settings, callib, img_size);
 
     } catch (const cv::Exception &exception) {
       cerr << "This was supposed to be the summer of George..." << endl;
