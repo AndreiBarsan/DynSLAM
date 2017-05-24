301e76b8e098201d016ec7462117dc905deac296
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2017-05-24 20:20:06 +0200

    Very early barely-working visualization in Pangolin.

--- src/DynSLAM/DynSLAMGUI.cpp
+++ src/DynSLAM/DynSLAMGUI.cpp
@@ -5,8 +5,10 @@
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include <backward.hpp>
+#include <GL/glut.h>
 
 #include "../InfiniTAM/InfiniTAM/ORUtils/CUDADefines.h"
+#include "ImageSourceEngine.h"
 
 // Handle SIGSEGV and its friends by printing sensible stack traces with code snippets.
 // TODO(andrei): this is a hack, please remove or depend on backward directly.
@@ -24,22 +26,128 @@ namespace gui {
 
 using namespace std;
 
-const int UI_WIDTH = 180;
+const int UI_WIDTH = 300;
 
+// TODO(andrei): Move away from here.
+/// \brief Main DynSLAM class interfacing between different submodules.
+class DynSlam {
+
+public:
+    void Initialize(ITMMainEngine *itm_static_scene_engine_, ImageSourceEngine *image_source) {
+
+      this->image_source_ = image_source;
+
+      window_size_.x = image_source->getDepthImageSize().x;
+      window_size_.y = image_source->getDepthImageSize().y;
+
+      this->itm_static_scene_engine_ = itm_static_scene_engine_;
+      this->current_frame_no_ = 0;
+
+//      int fake_argc = 0;
+//      glutInit(&fake_argc, nullptr);
+//      glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+//      glutInitWindowSize(window_size_.x, window_size_.y);
+//      glutCreateWindow("DynSLAM Lightweight GUI");
+
+      // TODO(andrei): Multiple panel support, if you don't go QT.
+//      glGenTextures(1, panel_texture_ids_);
+
+//      glutDisplayFunc(DynSlamGlutGui::GlutDisplay);
+//      glutKeyboardUpFunc(DynSlamGlutGui::GlutKeyboard);
+//      glutIdleFunc(DynSlamGlutGui::GlutIdle);
+
+      bool allocate_gpu = true;
+      Vector2i input_shape = image_source->getDepthImageSize();
+      out_image_ = new ITMUChar4Image(input_shape, true, allocate_gpu);
+      input_rgb_image_= new ITMUChar4Image(input_shape, true, allocate_gpu);
+      input_raw_depth_image_ = new ITMShortImage(input_shape, true, allocate_gpu);
+
+      // TODO(andrei): Own CUDA safety wrapper. With blackjack. And hookers.
+      ITMSafeCall(cudaThreadSynchronize());
+
+      cout << "DynSLAM initialization complete." << endl;
+    }
+
+  void ProcessFrame() {
+    if (! image_source_->hasMoreImages()) {
+      cout << "No more frames left in image source." << endl;
+      return;
+    }
+
+    // Read the images from the first part of the pipeline
+    image_source_->getImages(input_rgb_image_, input_raw_depth_image_);
+
+    // Forward them to InfiniTAM for the background reconstruction.
+    itm_static_scene_engine_->ProcessFrame(input_rgb_image_, input_raw_depth_image_);
+    ITMSafeCall(cudaThreadSynchronize());
+
+    current_frame_no_++;
+  }
+
+  int last_frame = -1;
+  const unsigned char* GetImageData() {
+    // TODO(andrei): Improve this crap.
+    // TODO(andrei): Get rid of reliance on itam enums.
+    if (last_frame != current_frame_no_ ) {
+      last_frame = current_frame_no_;
+      cout << "slam img size: " << itm_static_scene_engine_->GetImageSize() << endl;
+      itm_static_scene_engine_->GetImage(
+        out_image_,
+        ITMMainEngine::GetImageType::InfiniTAM_IMAGE_SCENERAYCAST);
+    }
+
+    return out_image_->GetData(MEMORYDEVICE_CPU)->getValues();
+  }
+
+  int GetInputWidth() {
+    return image_source_->getDepthImageSize().width;
+  }
+
+  int GetInputHeight() {
+    return image_source_->getDepthImageSize().height;
+  }
+
+private:
+  ITMLibSettings itm_lib_settings_;
+  // TODO(andrei): Write custom image source.
+  ImageSourceEngine *image_source_;
+
+  // This is the main reconstruction component. Should split for dynamic+static.
+  // In the future, we may need to write our own.
+  // For now, this shall only handle reconstructing the static part of a scene.
+  ITMMainEngine *itm_static_scene_engine_;
+
+  ITMUChar4Image *out_image_;
+  ITMUChar4Image *input_rgb_image_;
+  ITMShortImage  *input_raw_depth_image_;
+
+  int current_frame_no_;
+
+  Vector2i window_size_;
+
+//  uint panel_texture_ids_[1];
+//  bool needs_refresh_;
+//  Action next_action_;
+};
+
+/// TODO(andrei): Seriously consider using QT or wxWidgets. Pangolin is VERY limited in terms of the
+/// widgets it supports. It doesn't even seem to support multiline text, or any reasonable way to
+/// paint labels or lists or anything... Might be better not to worry too much about this, since
+/// there isn't that much time...
 class PangolinGui {
 public:
-  PangolinGui() {
+  PangolinGui(DynSlam *dyn_slam) : dyn_slam_(dyn_slam) {
     cout << "Pangolin GUI initialized." << endl;
 
-    // TODO(andrei): Put usefult things here.
+    // TODO(andrei): Put useful things in this config.
     // Load configuration data
 //    pangolin::ParseVarsFile("app.cfg");
-    // TODO(andrei): Set from input dataset.
-    width = 640;
-    height = 480;
+    // TODO(andrei): Proper scaling.
+    width = dyn_slam->GetInputWidth(); // / 1.5;
+    height = dyn_slam->GetInputHeight();// / 1.5;
 
-    CreatePangolinDisplays();
     SetupDummyImage();
+    CreatePangolinDisplays();
   }
 
   virtual ~PangolinGui() {
@@ -48,8 +156,64 @@ public:
 
   /// \brief Executes the main Pangolin input and rendering loop.
   void Run() {
-    // TODO move to own method
-    pangolin::CreateWindowAndBind("DynSLAM GUI", UI_WIDTH + width * 2, 480);
+    // Default hooks for exiting (Esc) and fullscreen (tab).
+    while (!pangolin::ShouldQuit()) {
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+      glColor3f(1.0, 1.0, 1.0);
+
+      // TODO(andrei): The buffers you're now allocating for intermediate results are very wasteful.
+      // It's almost 800Mb of VRAM just for panel stuff. Please fix this!
+      // TODO(andrei): Only use Pangolin camera if not PITA. Otherwise, can just base everything off
+      // InfiniTAM's raycasting.
+      main_view->Activate();
+      glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+      const unsigned char *slam_frame_data = dyn_slam_->GetImageData();
+
+//      UploadDummyTexture();
+//      glPixelStorei(GL_UNPACK_ALIGNMENT, (dummy_img.step & 3) ? 1 : 4);
+      //set length of one complete row in data (doesn't need to equal img.cols)
+//      glPixelStorei(GL_UNPACK_ROW_LENGTH, dummy_img.step/dummy_img.elemSize());
+//      slam_preview->Upload(dummy_img.data, GL_BGR, GL_UNSIGNED_BYTE);
+      slam_preview->Upload(slam_frame_data);
+      slam_preview->RenderToViewport(true);
+
+      rgb_view.Activate();
+      glColor3f(1.0, 1.0, 1.0);
+
+      // TODO(andrei): Undo these state changes, since they mess up the rest of the pipeline.
+//      UploadDummyTexture();
+//      dummy_image_texture->RenderToViewport();
+//
+//      depth_view.Activate();
+//      glColor3f(1.0, 0.0, 0.0);
+//      dummy_image_texture->RenderToViewport();
+//
+//      segment_view.Activate();
+//      glColor3f(0.0, 1.0, 0.0);
+//      dummy_image_texture->RenderToViewport();
+//
+//      object_view.Activate();
+//      glColor3f(0.0, 0.0, 1.0);
+//      dummy_image_texture->RenderToViewport();
+//
+//      glColor3f(1.0, 1.0, 1.0);
+//      pangolin::GlFont::I().Text("No data available").Draw(0,0,0);
+
+//      plotter.Activate();
+//      float val = static_cast<float>(sin(t));
+//      t += tinc;
+//      log.Log(val);
+
+      // Swap frames and Process Events
+      pangolin::FinishFrame();
+    }
+  }
+
+protected:
+
+  /// \brief Creates the GUI layout and widgets.
+  void CreatePangolinDisplays() {
+    pangolin::CreateWindowAndBind("DynSLAM GUI", UI_WIDTH + width * 2, height);
 
     // 3D Mouse handler requires depth testing to be enabled
     glEnable(GL_DEPTH_TEST);
@@ -65,13 +229,25 @@ public:
     pangolin::Var<function<void(void)>> a_button("ui.Some Button", []() {
       cout << "Clicked the button!" << endl;
     });
-    pangolin::Var<function<void(void)>> quite_button("ui.Quit Button", []() {
+    pangolin::Var<function<void(void)>> quit_button("ui.Quit Button", []() {
       pangolin::QuitAll();
+    });
+    pangolin::Var<string> reconstructions("ui.Reconstructions", "<none>");
+
+    pangolin::Var<function<void(void)>> previous_object("ui.Previous Object", []() {
+      cout << "Will select previous reconstructed object, once available..." << endl;
+    });
+    pangolin::Var<function<void(void)>> next_object("ui.Next Object", []() {
+      cout << "Will select next reconstructed object, once available..." << endl;
+    });
+    pangolin::RegisterKeyPressCallback('n', [&]() {
+      cout << "Next frame should happen now!" << endl;
+      dyn_slam_->ProcessFrame();
     });
 
     // Define Camera Render Object (for view / scene browsing)
-    pangolin::OpenGlMatrix proj = pangolin::ProjectionMatrix(640, 480, 420, 420, 320, 240, 0.1,
-                                                             1000);
+    pangolin::OpenGlMatrix proj = pangolin::ProjectionMatrix(
+      width, height, 420, 420, width / 2, height / 2, 0.1, 1000);
 
     float aspect_ratio = static_cast<float>(width) / height;
     rgb_view = pangolin::Display("rgb").SetAspect(aspect_ratio);
@@ -85,14 +261,14 @@ public:
     detail_views = &(pangolin::Display("detail"));
 
     main_view->SetAspect(aspect_ratio);
-    main_view_free_cam = pangolin::OpenGlRenderState(
-      proj, pangolin::ModelViewLookAt(0, 0, -2, 0, 0, 0, pangolin::AxisY));
+//    main_view_free_cam = pangolin::OpenGlRenderState(
+//      proj, pangolin::ModelViewLookAt(0, 0, -2, 0, 0, 0, pangolin::AxisY));
     detail_views->SetAspect(aspect_ratio);
 
     // TODO(andrei): Maybe wrap these guys?
     main_view->SetBounds(0.0, 1.0, pangolin::Attach::Pix(UI_WIDTH),
-                        pangolin::Attach::Pix(UI_WIDTH + width));
-    main_view->SetHandler(new pangolin::Handler3D(main_view_free_cam));
+                         pangolin::Attach::Pix(UI_WIDTH + width));
+//    main_view->SetHandler(new pangolin::Handler3D(main_view_free_cam));
 
     detail_views->SetBounds(0.0, 1.0, pangolin::Attach::Pix(width + UI_WIDTH), 1.0);
     detail_views->SetLayout(pangolin::LayoutEqual)
@@ -109,54 +285,10 @@ public:
     this->dummy_image_texture = new pangolin::GlTexture(
       george_width, george_height, GL_RGB, false, 0, GL_RGB, GL_UNSIGNED_BYTE);
 
-    // Default hooks for exiting (Esc) and fullscreen (tab).
-    while (!pangolin::ShouldQuit()) {
-      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-      glColor3f(1.0, 1.0, 1.0);
+    this->slam_preview = new pangolin::GlTexture(
+      width, height, GL_RGBA, false, 0, GL_RGBA, GL_UNSIGNED_BYTE);
 
-      // TODO(andrei): Only use Pangolin camera if not PITA. Otherwise, can just base everything off
-      // InfiniTAM's raycasting.
-      main_view->Activate(main_view_free_cam);
-      glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-      pangolin::glDrawColouredCube();
 
-      rgb_view.Activate();
-      glColor3f(1.0, 1.0, 1.0);
-
-      // Mess with George's bytes a little bit
-      //use fast 4-byte alignment (default anyway) if possible
-      glPixelStorei(GL_UNPACK_ALIGNMENT, (dummy_img.step & 3) ? 1 : 4);
-
-      //set length of one complete row in data (doesn't need to equal img.cols)
-      glPixelStorei(GL_UNPACK_ROW_LENGTH, dummy_img.step / dummy_img.elemSize());
-      dummy_image_texture->Upload(dummy_img.data, GL_BGR, GL_UNSIGNED_BYTE);
-      dummy_image_texture->RenderToViewport();
-
-      depth_view.Activate();
-      glColor3f(1.0, 0.0, 0.0);
-      dummy_image_texture->RenderToViewport();
-
-      segment_view.Activate();
-      glColor3f(0.0, 1.0, 0.0);
-      dummy_image_texture->RenderToViewport();
-
-      object_view.Activate();
-      glColor3f(0.0, 0.0, 1.0);
-      dummy_image_texture->RenderToViewport();
-
-//      plotter.Activate();
-//      float val = static_cast<float>(sin(t));
-//      t += tinc;
-//      log.Log(val);
-
-      // Swap frames and Process Events
-      pangolin::FinishFrame();
-    }
-  }
-
-protected:
-
-  void CreatePangolinDisplays() {
     // Custom: Add a plot to one of the views
     // Data logger object
 //    pangolin::DataLog log;
@@ -178,19 +310,13 @@ protected:
   }
 
   void SetupDummyImage() {
-
     cout << "Loading George..." << endl;
     dummy_img = cv::imread("/home/andrei/Pictures/george.jpg");
-
-//    // Mess with George's bytes a little bit
-//    //use fast 4-byte alignment (default anyway) if possible
-//    glPixelStorei(GL_UNPACK_ALIGNMENT, (dummy_img.step & 3) ? 1 : 4);
-//
-//    //set length of one complete row in data (doesn't need to equal img.cols)
-//    glPixelStorei(GL_UNPACK_ROW_LENGTH, dummy_img.step/dummy_img.elemSize());
   }
 
 private:
+  DynSlam *dyn_slam_;
+
   /// Input frame dimensions. They dictate the overall window size.
   int width, height;
 
@@ -202,16 +328,52 @@ private:
   pangolin::View object_view;
 
   pangolin::GlTexture *dummy_image_texture;
-  pangolin::OpenGlRenderState main_view_free_cam;
+  pangolin::GlTexture *slam_preview;
+//  pangolin::OpenGlRenderState main_view_free_cam;
 
   cv::Mat dummy_img;
+
+
+  void UploadDummyTexture() {
+    // Mess with George's bytes a little bit for OpenGL <-> OpenCV compatibility.
+    //use fast 4-byte alignment (default anyway) if possible
+    glPixelStorei(GL_UNPACK_ALIGNMENT, (dummy_img.step & 3) ? 1 : 4);
+    //set length of one complete row in data (doesn't need to equal img.cols)
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, dummy_img.step/dummy_img.elemSize());
+    dummy_image_texture->Upload(dummy_img.data, GL_BGR, GL_UNSIGNED_BYTE);
+  }
 };
 
 }
 }
 
 int main(int argc, char **argv) {
-  dynslam::gui::PangolinGui gui;
-  gui.Run();
+  using namespace dynslam;
+  gui::DynSlam *dyn_slam = new gui::DynSlam();
+  const string dataset_root = "/home/andrei/work/libelas/cmake-build-debug/odo_seq_06/";
+  const string calib_fpath = dataset_root + "calib.txt";
+  const string rgb_image_format = dataset_root + "Frames/%04i.ppm";
+  const string depth_image_format = dataset_root + "Frames/%04i.pgm";
+  ImageSourceEngine *image_source = new ImageFileReader(
+    calib_fpath.c_str(),
+    rgb_image_format.c_str(),
+    depth_image_format.c_str()
+  );
+
+  ITMLibSettings *settings = new ITMLibSettings();
+
+  ITMMainEngine *static_scene_engine = new ITMMainEngine(
+    settings,
+    new ITMRGBDCalib(image_source->calib),
+    image_source->getRGBImageSize(),
+    image_source->getDepthImageSize()
+  );
+
+  dyn_slam->Initialize(static_scene_engine, image_source);
+
+  gui::PangolinGui pango_gui(dyn_slam);
+  pango_gui.Run();
+
+  delete dyn_slam;
 }
 
