2daf300baac7ac6c185e8fcda15cffea3d65bb15
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2017-05-26 17:50:41 +0200

    Moved lots of functionality to dynslam.

--- src/DynSLAM/InfiniTamDriver.cpp
+++ src/DynSLAM/InfiniTamDriver.cpp
@@ -10,9 +10,24 @@ InfiniTamDriver::InfiniTamDriver(const ITMLibSettings *settings,
                                  const Vector2i &imgSize_d)
   : ITMMainEngine(settings, calib, imgSize_rgb, imgSize_d) {}
 
+// TODO(andrei): Get rid of these notes.
+// We will likely want different alignment techniques for the background and for the dynamic
+// objects. Without the 'GetImage' stuff, the ITMMainEngine code is ~150 LOC. Their architecture is
+// very clean; the main engine only connects other core components. We could therefore simply just
+// use their denseMapper directly, and solve the other bits ourselves: ground truth or sparse+dense
+// VO for the background, SF-based+dense VO for the individual objects, etc.
+
+
 void InfiniTamDriver::ProcessFrame(ITMUChar4Image *rgbImage, ITMShortImage *rawDepthImage,
                                    ITMIMUMeasurement *imuMeasurement) {
-  ITMMainEngine::ProcessFrame(rgbImage, rawDepthImage, imuMeasurement);
+  if (imuMeasurement != nullptr) {
+    throw runtime_error("IMU data integration not supported.");
+  }
+
+  this->UpdateView(rgbImage, rawDepthImage);
+  this->Track();
+  this->Integrate();
+  this->PrepareNextStep();
 }
 
 
