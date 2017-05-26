44c45f03408066a4d51915b82e7dfb26e25129f6
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2017-05-26 11:48:37 +0200

    Tweaked build.

---
+++ cmake/Utils.cmake
@@ -0,0 +1,8 @@
+# A series of small utilities useful for e.g., debugging CMake builds.
+
+# Use this to print all available CMake variables.
+#get_cmake_property(_variableNames VARIABLES)
+#foreach (_variableName ${_variableNames})
+#  message(STATUS "${_variableName}=${${_variableName}}")
+#endforeach()
+
