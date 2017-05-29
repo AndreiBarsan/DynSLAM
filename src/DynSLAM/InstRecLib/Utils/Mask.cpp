b2eb822aa39524e7af9d37bc36f9b04425d57001
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2017-05-29 11:47:11 +0200

    Switched to using sane opencv to manage mask data for the segmentation, instead of raw arrays.

--- src/DynSLAM/InstRecLib/Utils/Mask.cpp
+++ src/DynSLAM/InstRecLib/Utils/Mask.cpp
@@ -5,32 +5,13 @@
 namespace instreclib {
 namespace utils {
 
-void Mask::Clear() {
-  if (mask_) {
-    // TODO(andrei): Handle the mask in a more modern way!
-    for (int row = 0; row < GetHeight(); ++row) {
-      if (mask_[row]) {
-        delete[] mask_[row];
-      }
-    }
-    delete[] mask_;
-  }
-}
-
 void Mask::Set(const Mask &rhs) {
   // Handle the bounding box
   this->bounding_box_ = rhs.bounding_box_;
 
   // Handle the detailed pixel-wise mask
-  int height = rhs.GetHeight();
-  int width = rhs.GetWidth();
-  this->Clear();
-
-  this->mask_ = new uint8_t *[height];
-  for (int i = 0; i < height; ++i) {
-    this->mask_[i] = new uint8_t[width];
-    std::memcpy(this->mask_[i], rhs.mask_[i], width * sizeof(uint8_t));
-  }
+  delete mask_data_;
+  mask_data_ = new cv::Mat(*rhs.mask_data_);
 }
 
 }   // namespace utils
