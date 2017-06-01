aff6b42f4a1d13ec01d3416e7c341790062e0e60
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2017-06-01 15:45:44 +0200

    Managed to fuck RGB+depth input

--- src/DynSLAM/Input.cpp
+++ src/DynSLAM/Input.cpp
@@ -3,5 +3,12 @@
 
 namespace dynslam {
 
-
+ITMLib::Objects::ITMRGBDCalib ReadITMCalibration(const std::string &fpath) {
+  ITMLib::Objects::ITMRGBDCalib out_calib;
+  if (! ITMLib::Objects::readRGBDCalib(fpath.c_str(), out_calib)) {
+    throw std::runtime_error(dynslam::utils::Format(
+        "Could not read calibration file: [%s]\n", fpath));
+  }
+  return out_calib;
+}
 }
