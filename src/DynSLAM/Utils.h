8399f0f03bc13704fdf443a7eb4426bae488d03f
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2017-06-01 19:35:21 +0200

    Some cleanup

--- src/DynSLAM/Utils.h
+++ src/DynSLAM/Utils.h
@@ -2,15 +2,26 @@
 #define DYNSLAM_UTILS_H
 
 #include <string>
+#include <sys/stat.h>
 
-namespace dynslam { namespace utils {
+#include <highgui.h>
+
+#include "DepthEngine.h"
+
+namespace dynslam {
+namespace utils {
 
 bool EndsWith(const std::string &value, const std::string &ending);
 
 /// \brief Streamlined 'sprintf' functionality for C++.
 std::string Format(const std::string& fmt, ...);
 
-}}
+inline bool file_exists(const std::__cxx11::string& name) {
+  struct stat buffer;
+  return stat(name.c_str(), &buffer) == 0;
+}
 
+} // namespace utils
+} // namespace dynslam
 
 #endif //DYNSLAM_UTILS_H
