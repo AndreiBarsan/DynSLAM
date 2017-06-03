6f172e2fe028f017d7a94b6c0d7104e14cd730f6
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2017-06-03 14:47:50 +0200

    Can now (almost) easily toggle between libelas and dispnet precomputed depth map usage.

--- src/DynSLAM/DepthEngine.h
+++ src/DynSLAM/DepthEngine.h
@@ -2,16 +2,21 @@
 #ifndef DYNSLAM_DEPTHENGINE_H
 #define DYNSLAM_DEPTHENGINE_H
 
+#include <limits>
+
 #include <opencv/cv.h>
 #include "Utils.h"
 
-namespace dynslam { namespace utils { // voodoo
-std::string type2str(int type);
+// Some necessary forward declarations
+namespace dynslam { namespace utils {
+  std::string type2str(int type);
+  std::string Format(const std::string& fmt, ...);
 }}
 
 namespace dynslam {
 
-/// \brief Contains the calibration parameters of a stereo rig, such as the AnnieWAY platform.
+/// \brief Contains the calibration parameters of a stereo rig, such as the AnnieWAY platform,
+/// which is the one used to record the KITTI dataset.
 struct StereoCalibration {
   float baseline_meters;
   float focal_length_px;
@@ -27,7 +32,39 @@ class DepthEngine {
  public:
   virtual ~DepthEngine() {}
 
-  /// \brief Computes a disparity map from
+  /// \brief Computes a depth map from a stereo image pair (stereo -> disparity -> depth).
+  void DepthFromStereo(const cv::Mat &left,
+                       const cv::Mat &right,
+                       const StereoCalibration &calibration,
+                       cv::Mat_<uint16_t> &out_depth) {
+    if(input_is_depth_) {
+      DisparityMapFromStereo(left, right, out_depth);
+      return;
+    }
+
+    // TODO(andrei): Consider reusing this buffer.
+    cv::Mat out_disparity;
+    DisparityMapFromStereo(left, right, out_disparity);
+
+    // ...and this one?
+    out_depth = cv::Mat_<uint16_t>(out_disparity.size(), CV_16UC1);
+
+    // This should be templated in a nicer fashion...
+    if (out_disparity.type() == CV_32FC1) {
+      DepthFromDisparityMap<float>(out_disparity, calibration, out_depth);
+    }
+    else if (out_disparity.type() == CV_16UC1) {
+      DepthFromDisparityMap<uint16_t>(out_disparity, calibration, out_depth);
+    }
+    else {
+      throw std::runtime_error(utils::Format(
+          "Unknown data type for disparity matrix [%s]. Supported are CV_32FC1 and CV_16UC1.",
+          utils::type2str(out_disparity.type()).c_str()
+      ));
+    }
+  }
+
+  /// \brief Computes a disparity map from a stereo image pair.
   virtual void DisparityMapFromStereo(const cv::Mat &left,
                                       const cv::Mat &right,
                                       cv::Mat &out_disparity) = 0;
@@ -41,38 +78,36 @@ class DepthEngine {
   // TODO(andrei): This can be sped up trivially using CUDA.
   /// \brief Computes a depth map from a disparity map using the `DepthFromDisparity` function at
   /// every pixel.
-  virtual void DepthFromDisparityMap(const cv::Mat &disparity,
-                                     const StereoCalibration &calibration,
-                                     cv::Mat &out_depth) {
+  template<typename T>
+  void DepthFromDisparityMap(const cv::Mat_<T> &disparity,
+                             const StereoCalibration &calibration,
+                             cv::Mat_<uint16_t> &out_depth) {
     assert(disparity.size() == out_depth.size());
-    using namespace std;
 
-    cout << disparity.size() << out_depth.size() << endl;
-    cout << disparity.elemSize() << " " << disparity.type() << " "
-         << dynslam::utils::type2str(disparity.type()) << endl;
     for(int i = 0; i < disparity.rows; ++i) {
       for(int j = 0; j < disparity.cols; ++j) {
-//        cout << i << ", " << j << " " << disparity.cols << " x " << disparity.cols << endl;
-        // TODO correctly use the right way depending on whether the input image is short or float
-        float disp = disparity.at<float>(i, j);
+        // BURN THE WITCH
+        T disp = disparity.template at<T>(i, j);
 
-        int32_t disp_long = static_cast<int32_t>(1000.0 * DepthFromDisparity(disp, calibration));
-        if (disp_long > 20 * 1000 || disp_long < 1000) {
-          disp_long = numeric_limits<uint16_t>::max();
+        int32_t depth_long = static_cast<int32_t>(1000.0 * DepthFromDisparity(disp, calibration));
+        const int32_t kMaxDepthMeters = 25;
+
+        if (depth_long > kMaxDepthMeters * 1000 || depth_long < 0) {
+          depth_long = std::numeric_limits<uint16_t>::max();
         }
-////
-        uint16_t depth_s = static_cast<uint16_t>(disp_long);
-        // Use for oldschoold pgm depth files
-//        uint16_t depth_s = disparity.at<uint16_t>(i, j);
+
+        uint16_t depth_s = static_cast<uint16_t>(depth_long);
         out_depth.at<uint16_t>(i, j) = depth_s;
       }
     }
-    cout << "Loop done." << endl;
-
   }
 
  protected:
-  DepthEngine() {}
+  DepthEngine(bool input_is_depth) : input_is_depth_(input_is_depth) {}
+
+  /// \brief If true, then assume the read maps are depth maps, instead of disparity maps.
+  /// In this case, the depth from disparity computation is no longer performed.
+  bool input_is_depth_;
 };
 
 } // namespace dynslam
