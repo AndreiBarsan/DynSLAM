84d80996a30c39fc18bceb0ab4007401b6f65bdc
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2017-06-03 14:47:50 +0200

    Can now (almost) easily toggle between libelas and dispnet precomputed depth map usage.

--- src/DynSLAM/PrecomputedDepthEngine.h
+++ src/DynSLAM/PrecomputedDepthEngine.h
@@ -7,16 +7,18 @@
 
 namespace dynslam {
 
-/// \brief Reads precomputed depth maps from a folder.
+/// \brief Reads precomputed disparity (default) or depth maps from a folder.
 /// The depth maps are expected to be grayscale, and in 8 or 16-bit format if 'hdr_depth_' if
 /// false, and in 32-bit PFM format otherwise.
 class PrecomputedDepthEngine : public DepthEngine {
  public:
   virtual ~PrecomputedDepthEngine() {}
 
+  // TODO(andrei): Do we still need the HDR flag?
   PrecomputedDepthEngine(const std::string &folder, const std::string &fname_format,
-                         bool hdr_depth = false)
-      : folder(folder),
+                         bool hdr_depth = false, bool input_is_depth = false)
+      : DepthEngine(input_is_depth),
+        folder(folder),
         fname_format(fname_format),
         frame_idx(0),
         hdr_depth_(hdr_depth) {}
@@ -34,7 +36,10 @@ class PrecomputedDepthEngine : public DepthEngine {
   /// which are called "frame-0000.png"-"frame-9999.png".
   std::string fname_format;
   int frame_idx;
+
+  /// \brief Whether the depth is in HDR format (32-bit float). Otherwise, assumes 16-bit integers.
   bool hdr_depth_;
+
 };
 
 } // namespace dynslam
