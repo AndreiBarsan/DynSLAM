148ed2354c3069ed7d8404dd32ec88af9068c9ab
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2017-06-20 12:59:12 +0200

    Minor script reorg

---
+++ scripts/prepare-odometry.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# A script for preparing a KITTI odometry sequence for DynSLAM.
+# TODO(andrei): Set up calibration files correctly and finish implementing.
+
+set -euo pipefail
+IFS=$'\n\t'
+
+usage() {
+  echo >&2 "Usage: $0 <kitti-odometry-dataset-root>"
+}
+
+if [[ "$#" -ne 1 ]]; then
+  usage;
+  exit 1;
+fi
+
+DATASET_ROOT="$1"
+SEQS="${DATASET_ROOT}/sequences"
+POSES="${DATASET_ROOT}/poses"
+
+echo "Dataset root: $DATASET_ROOT"
+
+if ! [[ -d "${SEQS}" ]]; then
+  echo >&2 "No sequences directory found. Looking for: ${SEQS}"
+  exit 2;
+fi
+
+if ! [[ -d "${POSES}" ]]; then
+  echo >&2 "No ground truth poses directory found. Looking for: ${POSES}"
+  exit 3;
+fi
+
+for SEQ_DIRNAME in $(ls "${SEQS}"); do
+  GT_POSES="${POSES}/${SEQ_DIRNAME}.txt"
+  SEQ_DIR="${SEQS}/${SEQ_DIRNAME}"
+  echo "Processing sequence ${SEQ_DIRNAME}."
+  if [[ -f "${GT_POSES}" ]]; then
+    ln -sf "${GT_POSES}" "${SEQ_DIR}/ground-truth-poses.txt"
+  else
+    echo "Warning: No ground truth poses available for sequence [${SEQ_DIR}]."
+  fi
+done
+
+echo "Done."
+
