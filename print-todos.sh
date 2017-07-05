ded4e67f45ace8fea3a13bfeee2a882171df3a85
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2017-07-05 16:21:34 +0200

    Improved TODO printing script; worked on ITM

--- print-todos.sh
+++ print-todos.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ag '(TODO|TOOD|FIXME|XXX)' src --before=1 --after=3 --ignore-case "$@"
+ag --before=1 --after=3 --ignore-case "$@" '(TODO|TOOD|FIXME|XXX)' src/InfiniTAM src/DynSLAM
