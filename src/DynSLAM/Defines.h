ecb6989f762d0443d2297e0e67aa85922df35b31
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2017-08-02 21:15:21 +0200

    Calibration stuff; started work towards proper reconstruction quality evaluation

--- src/DynSLAM/Defines.h
+++ src/DynSLAM/Defines.h
@@ -4,8 +4,14 @@
 
 #include <Eigen/Core>
 
-// Necessary for having Eigen types as fields.
+// Necessary for having Eigen types as fields, but a little more readable for people less familiar
+// with Eigen. More information can be found here: https://eigen.tuxfamily.org/dox/group__TopicStructHavingEigenMembers.html
 #define SUPPORT_EIGEN_FIELDS EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
 
+namespace Eigen {
+  /// Simplify code dealing with projection matrices.
+  using Matrix34d = Matrix<double, 3, 4>;
+  using Matrix34f = Matrix<float, 3, 4>;
+}
 
 #endif //DYNSLAM_DSDEFINES_H
