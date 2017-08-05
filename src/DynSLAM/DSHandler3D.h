27d8860ae6c76efbba09137f691ff7c84f198d4f
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2017-08-05 17:41:15 +0200

    Worked on better camera

--- src/DynSLAM/DSHandler3D.h
+++ src/DynSLAM/DSHandler3D.h
@@ -12,17 +12,46 @@ namespace gui {
 /// do, which is preferred when visualizing large reconstructions and mixed raycast-raster renders.
 class DSHandler3D : public pangolin::Handler {
  public:
+  static Eigen::Vector3d GetTranslation(const pangolin::OpenGlRenderState &state) {
+    Eigen::Vector3d trans;
+    trans(0) = state.GetModelViewMatrix()(0, 2);
+    trans(1) = state.GetModelViewMatrix()(1, 2);
+    trans(2) = state.GetModelViewMatrix()(2, 2);
+    return trans;
+  }
+
+  static Eigen::Vector3d GetDirection(const pangolin::OpenGlRenderState &state) {
+    Eigen::Matrix4d mv = state.GetModelViewMatrix();
+    return mv.block(0, 0, 3, 3) * Eigen::Vector3d(0, 0, 1.0);
+  }
+
+  static Eigen::Vector3d GetEuler(const Eigen::Matrix3d &rot) {
+    return rot.eulerAngles(0, 1, 2);
+  }
+
+ public:
   DSHandler3D(pangolin::OpenGlRenderState *cam_state,
               pangolin::AxisDirection enforce_up,
               float trans_scale,
-              float zoom_fraction)
-    : pangolin::Handler(),
-      eye(-0.3, 0.4, 3.0),
-      direction(0, 0, -1),
+              float zoom_scale)
+    : eye(GetTranslation(*cam_state)),
+      direction(GetDirection(*cam_state)),
       enforce_up_(enforce_up),
       cam_state_(cam_state),
-      last_pos_{0.0f, 0.0f}
+      last_pos_{0.0f, 0.0f},
+      trans_rot_scale_{trans_scale},
+      zoom_scale_{zoom_scale}
   {
+    Eigen::Matrix4d mv = cam_state->GetModelViewMatrix();
+    Eigen::Vector3d euler = GetEuler(mv.block(0, 0, 3, 3));
+
+    // XXX: no more degrees bro
+    yaw_accum_ = euler(1) * 180 / M_PI;
+    pitch_accum_ = euler(0) * 180 / M_PI;
+
+    using namespace std;
+    cout << "EULER: " << euler << endl;
+
     UpdateModelViewMatrix();
   }
 
@@ -46,8 +75,11 @@ class DSHandler3D : public pangolin::Handler {
   pangolin::OpenGlRenderState *cam_state_;
   float last_pos_[2];
 
-  float yaw_accum = 0.0f;
-  float pitch_accum = 0.0f;
+  float yaw_accum_ = 0.0f;
+  float pitch_accum_ = 0.0f;
+
+  float trans_rot_scale_;
+  float zoom_scale_;
 };
 
 } // namespace gui
