cc2e55f3b9fe493d150a594e4c4d0e6e8612819d
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2017-08-05 16:59:09 +0200

    Support for rasterization on top of the raycast result; now also rendering the pose history in the main view

--- src/DynSLAM/DSHandler3D.cpp
+++ src/DynSLAM/DSHandler3D.cpp
@@ -6,110 +6,73 @@
 namespace dynslam {
 namespace gui {
 
+using namespace std;
+
 void DSHandler3D::UpdateModelViewMatrix() {
   Eigen::Vector3d target = eye + direction;
-  cam_state->SetModelViewMatrix(pangolin::ModelViewLookAt(eye[0], eye[1], eye[2],
+  cam_state_->SetModelViewMatrix(pangolin::ModelViewLookAt(eye[0], eye[1], eye[2],
                                  target[0], target[1], target[2],
-                                 enforce_up));
+                                 enforce_up_));
 }
 
 void DSHandler3D::MouseMotion(pangolin::View &view, int x, int y, int button_state) {
-//    pangolin::Handler3D::MouseMotion(view, x, y, button_state);
-  // Hack: copied Handler3D's method and modified it directly. There has to be a better way to
-  // make this nicer...
   using namespace pangolin;
 
-  const GLprecision rf = 0.01;
-  const float delta[2] = {(float) x - last_pos[0], (float) y - last_pos[1]};
+  const float delta[2] = {(float) x - last_pos_[0], (float) y - last_pos_[1]};
   const float mag = delta[0] * delta[0] + delta[1] * delta[1];
 
   if (mag < 50.0f * 50.0f) {
-    OpenGlMatrix &mv = cam_state->GetModelViewMatrix();
-
-    GLprecision current_trans[3];
-    LieSetTranslation(mv.m, current_trans);
-
-    const GLprecision *up = AxisDirectionVector[enforce_up];
-//    GLprecision T_nc[3 * 4];
-//    LieSetIdentity(T_nc);
-//    bool rotation_changed = false;
-
-    if (button_state == MouseButtonMiddle) {
-      // ignore for now
-
-    } else if (button_state == MouseButtonLeft) {
+    const GLprecision *up = AxisDirectionVector[enforce_up_];
+    if (button_state == MouseButtonLeft) {
       // Left Drag: in plane translate
-
-      // Act like WASD
       Eigen::Vector3d up_v(up[0], up[1], up[2]);
 
       float dx = delta[0];
-      float dy = delta[1];
+      float dy = -delta[1];
 
       Eigen::Vector3d newMotionX = direction.cross(up_v).normalized() * dx * 0.05f;
       Eigen::Vector3d newMotionY = direction.cross(up_v).cross(direction).normalized() * dy * 0.05f;
       eye += newMotionX;
       eye += newMotionY;
 
-
       UpdateModelViewMatrix();
 
-
-
-      /*
-      if (ValidWinDepth(last_z)) {
-        GLprecision np[3];
-        PixelUnproject(view, x, y, last_z, np);
-        float kDragSpeedupFactor = 2.5f;
-//          const GLprecision t[] = { np[0] - rot_center[0], np[1] - rot_center[1], 0};
-        const GLprecision t[] = {(-np[0] + rot_center[0]) * 100 * kDragSpeedupFactor * tf,
-                                 (np[1] - rot_center[1]) * 100 * kDragSpeedupFactor * tf,
-                                 0};
-        LieSetTranslation<>(T_nc, t);
-        std::copy(np, np + 3, rot_center);
-      } else {
-//          const GLprecision t[] = { -10*delta[0]*tf, 10*delta[1]*tf, 0};
-        const GLprecision t[] = {10 * delta[0] * tf, 10 * delta[1] * tf, 0};    // Removed a -
-        LieSetTranslation<>(T_nc, t);
-      }
-       */
-    } else if (button_state == MouseButtonRight) {
-      /*
-      GLprecision aboutx = -rf * delta[1];
-      GLprecision abouty = -rf * delta[0];
-
-      if (enforce_up) {
-        // Special case if view direction is parallel to up vector
-        const GLprecision updotz = mv.m[2] * up[0] + mv.m[6] * up[1] + mv.m[10] * up[2];
-        if (updotz > 0.98) aboutx = std::min(aboutx, (GLprecision) 0.0);
-        if (updotz < -0.98) aboutx = std::max(aboutx, (GLprecision) 0.0);
-        // Module rotation around y so we don't spin too fast!
-        abouty *= (1 - 0.6 * fabs(updotz));
-      }
-
-      // Right Drag: object centric rotation
-      GLprecision T_2c[3 * 4];
-      Rotation<>(T_2c, aboutx, abouty, (GLprecision) 0.0);
-      GLprecision mrotc[3];
-      MatMul<3, 1>(mrotc, rot_center, (GLprecision) -1.0);
-      LieApplySO3<>(T_2c + (3 * 3), T_2c, mrotc);
-      GLprecision T_n2[3 * 4];
-      LieSetIdentity<>(T_n2);
-      LieSetTranslation<>(T_n2, rot_center);
-      LieMulSE3(T_nc, T_n2, T_2c);
-      rotation_changed = true;
-       */
     }
+    else if (button_state == MouseButtonRight) {
+      GLprecision aboutx = -0.3 * delta[0];
+      GLprecision abouty = -0.3 * delta[1];
 
-//    LieMul4x4bySE3<>(mv.m, T_nc, mv.m);
+      yaw_accum -= aboutx;
+      pitch_accum -= abouty;
 
-//    if (enforce_up != AxisNone && rotation_changed) {
-//      EnforceUpT_cw(mv.m, up);
-//    }
+      while (yaw_accum < -360.0) { yaw_accum += 360.0f; }
+      while (yaw_accum > +360.0) { yaw_accum -= 360.0f; }
+
+      float pitch_lim = 88.9f;
+      if (pitch_accum > pitch_lim) {
+        pitch_accum = pitch_lim;
+      }
+      if (pitch_accum < -pitch_lim) {
+        pitch_accum = -pitch_lim;
+      }
+
+      // Do NOT do a barrel roll. In fact, don't roll at all because it's not really useful when
+      // flying through a scene.
+      double roll = 0.0f;
+      double TO_RAD = M_PI / 180.0f;
+      // The ordering of the rotation angles is specific to InfiniTAM's axis conventions.
+      Eigen::Quaterniond rot_quat = Eigen::AngleAxisd(roll * TO_RAD, Eigen::Vector3d::UnitX())
+          * Eigen::AngleAxisd(yaw_accum * TO_RAD, Eigen::Vector3d::UnitY())
+          * Eigen::AngleAxisd(pitch_accum * TO_RAD, Eigen::Vector3d::UnitZ());
+
+      direction = rot_quat * Eigen::Vector3d(1.0f, 0.0f, 0.0f);
+
+      UpdateModelViewMatrix();
+    }
   }
 
-  last_pos[0] = (float) x;
-  last_pos[1] = (float) y;
+  last_pos_[0] = (float) x;
+  last_pos_[1] = (float) y;
 }
 
 void DSHandler3D::Mouse(pangolin::View &view,
@@ -126,23 +89,21 @@ void DSHandler3D::Mouse(pangolin::View &view,
     button_state |= pangolin::MouseButtonRight;
   }
 
-  last_pos[0] = static_cast<float>(x);
-  last_pos[1] = static_cast<float>(y);
+  float scroll_factor = 0.5f;
+
+  last_pos_[0] = static_cast<float>(x);
+  last_pos_[1] = static_cast<float>(y);
 
   if (pressed) {
     if (button == pangolin::MouseWheelUp) {
-      eye += direction.normalized() * 0.5;
+      eye += direction.normalized() * scroll_factor;
     }
     else if(button == pangolin::MouseWheelDown) {
-      eye -= direction.normalized() * 0.5;
+      eye -= direction.normalized() * scroll_factor;
     }
 
     UpdateModelViewMatrix();
   }
-
-
-  // would otherwise handle scrolling
-//  pangolin::Handler3D::Mouse(view, button, x, y, pressed, button_state);
 }
 
 } // namespace gui
